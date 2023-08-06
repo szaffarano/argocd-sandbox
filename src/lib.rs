05caa2f82e3c6267a4c980d158ce935e320307b7
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-08-06 16:39:56 +0200

    Simplify code

--- src/lib.rs
+++ src/lib.rs
@@ -1,85 +1,21 @@
-#![allow(unused_variables)]
+pub fn greet(name: String) -> String {
+    match name.as_str() {
+        "" => "Hello unknown!".to_string(),
+        _ => format!("Hello, {}!", name)
 
-use std::{
-    sync::{mpsc, Arc, Mutex},
-    thread,
-};
-
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: Option<mpsc::Sender<Job>>,
-}
-
-type Job = Box<dyn FnOnce() + Send + 'static>;
-
-impl ThreadPool {
-    pub fn build(size: usize) -> Result<ThreadPool, &'static str> {
-        if size <= 0 {
-            return Err("Pool size must be greater than 0");
-        }
-        let (sender, receiver) = mpsc::channel();
-        let receiver = Arc::new(Mutex::new(receiver));
-
-        let mut workers = Vec::with_capacity(size);
-
-        for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
-        }
-
-        Ok(ThreadPool {
-            workers,
-            sender: Some(sender),
-        })
-    }
-
-    pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
-        let job = Box::new(f);
-
-        self.sender.as_ref().unwrap().send(job).unwrap();
     }
 }
 
-impl Drop for ThreadPool {
-    fn drop(&mut self) {
-        drop(self.sender.take());
-
-        for worker in &mut self.workers {
-            println!("Shutting down worker {}", worker.id);
-
-            if let Some(thread) = worker.thread.take() {
-                thread.join().unwrap();
-            }
-        }
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_greet() {
+        assert_eq!("Hello, world!", super::greet("world".to_string()));
     }
-}
 
-struct Worker {
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
-}
-
-impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            let msg = receiver.lock().unwrap().recv();
-
-            match msg {
-                Ok(job) => {
-                    job();
-                }
-                Err(_) => {
-                    println!("Worker {id} disconnected; shutting down.");
-                    break;
-                }
-            }
-        });
-
-        Worker {
-            id,
-            thread: Some(thread),
-        }
+    #[test]
+    fn test_greet_with_empty_string() {
+        assert_eq!("Hello unknown!", super::greet("".to_string()));
+    
     }
 }
