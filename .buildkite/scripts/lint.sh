892b3aa153e9c606df35e6c1e30491c4fd2a74f8
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-08-12 18:44:30 +0200

    Test echoing

--- .buildkite/scripts/lint.sh
+++ .buildkite/scripts/lint.sh
@@ -6,6 +6,8 @@ echo "---------- env ----------"
 env
 echo "---------- /env ----------"
 
+echo "------- I should not show GITHUB_TOKEN:${GITHUB_TOKEN}!!!"
+
 rustup component add rustfmt
 
 cargo fmt --all -- --check
