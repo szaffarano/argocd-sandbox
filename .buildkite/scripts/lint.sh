8dfcffc2ea17c8a1d9299753531d5bb49f43517b
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-08-13 15:04:09 +0200

    Add more linting stuf

--- .buildkite/scripts/lint.sh
+++ .buildkite/scripts/lint.sh
@@ -3,5 +3,7 @@
 set -euo pipefail
 
 go install golang.org/x/lint/golint@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 
 $HOME/go/bin/golint ./...
+$HOME/go/bin/golangci-lint run
