62f8c4b07fb08d5808544d62118d1b4467d4cd33
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-08-13 15:52:47 +0200

    Fix image tag

--- .buildkite/scripts/deploy.sh
+++ .buildkite/scripts/deploy.sh
@@ -9,13 +9,12 @@ GIT_COMMIT="$(git rev-parse --short HEAD)"
 VERSION="$(cat .version)"
 IMAGE="szaffarano/argocd-sandbox"
 
-
 docker build \
 	-t "$IMAGE:latest" \
 	--build-arg VERSION="$VERSION" \
 	--build-arg GIT_COMMIT="$GIT_COMMIT" .
 
-docker tag "$IMAGE:latest" "$IMAGE:$VERSION"
+docker tag "$IMAGE:latest" "$IMAGE:$GIT_COMMIT"
 
 echo $DOCKERHUB_TOKEN | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
