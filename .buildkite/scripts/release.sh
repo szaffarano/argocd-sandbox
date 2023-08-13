c730e57cc255a97f8c8410e420456528e56f91ac
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-08-13 16:29:59 +0200

    Update release script

--- .buildkite/scripts/release.sh
+++ .buildkite/scripts/release.sh
@@ -2,4 +2,22 @@
 
 set -euo pipefail
 
-echo "Not implemented"
+branch="release/$(cat .version)"
+
+local remote=$(
+	git remote get-url origin | sed -E s"/https:\/\/(.*)/https:\/\/${GITHUB_TOKEN}@\1/g"
+)
+
+git config --global user.email "no-reply@ci-bot"
+git config --global user.name "CI Bot"
+git remote set-url origin "$remote"
+
+git checkout -b "$branch"
+echo "test" > flag
+git add flag
+git commit -m "Automated CI/CD commit"
+git push origin "$branch"
+
+gh pr create -b "Please review" -t "Bump version to $(cat .version)" -H master
+
+rm remote
