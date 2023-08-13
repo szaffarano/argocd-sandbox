26436d9e179e60ae6390d52595019f10edaf4845
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-08-13 15:35:35 +0200

    Enable lint

--- .buildkite/scripts/lint.sh
+++ .buildkite/scripts/lint.sh
@@ -2,10 +2,5 @@
 
 set -eo pipefail
 
-if [ -n "$RUN_LINT" ]; then
-	go install golang.org/x/lint/golint@latest
-	go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
-
-	$HOME/go/bin/golint ./...
-	$HOME/go/bin/golangci-lint run
-fi
+golint ./...
+golangci-lint run
