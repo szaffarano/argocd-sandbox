040acfc020059b09017651ac543da058be829429
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-08-13 18:36:50 +0200

    Update CI commit message

--- .buildkite/scripts/release.sh
+++ .buildkite/scripts/release.sh
@@ -29,7 +29,7 @@ git remote set-url origin "$remote"
 git checkout -b "$branch"
 echo "test" >flag
 git add flag
-git commit -m "Automated CI/CD commit"
+git commit -m "Automated CI/CD commit" -m "[skip ci]"
 git push origin "$branch"
 
 gh pr create -b "Please review" -t "Bump version to $(cat .version)"
