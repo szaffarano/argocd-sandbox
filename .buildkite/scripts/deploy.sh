ce340a27841ded5169d90d1c8f71152895ed5621
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-08-13 18:44:17 +0200

    pre commit hooks (#16)

--- .buildkite/scripts/deploy.sh
+++ .buildkite/scripts/deploy.sh
@@ -4,20 +4,20 @@ set -euo pipefail
 
 [ -d "$HOME" ] || mkdir "$HOME"
 
-git config --global --add safe.directory $(pwd)
+git config --global --add safe.directory "$(pwd)"
 
 GIT_COMMIT="$(git rev-parse --short HEAD)"
 VERSION="$(cat .version)"
 IMAGE="szaffarano/argocd-sandbox"
 
 docker build \
-	-t "$IMAGE:latest" \
-	--build-arg VERSION="$VERSION" \
-	--build-arg GIT_COMMIT="$GIT_COMMIT" .
+  -t "$IMAGE:latest" \
+  --build-arg VERSION="$VERSION" \
+  --build-arg GIT_COMMIT="$GIT_COMMIT" .
 
 docker tag "$IMAGE:latest" "$IMAGE:$GIT_COMMIT"
 
-echo $DOCKERHUB_TOKEN | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
 docker push "$IMAGE:latest"
 docker push "$IMAGE:$GIT_COMMIT"
--- .buildkite/scripts/release.sh
+++ .buildkite/scripts/release.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 [ -d "$HOME" ] || mkdir "$HOME"
 
-git config --global --add safe.directory $(pwd)
+git config --global --add safe.directory "$(pwd)"
 
 GIT_COMMIT="$(git rev-parse --short HEAD)"
 VERSION="$(cat .version)"
@@ -13,10 +13,10 @@ IMAGE="szaffarano/argocd-sandbox"
 branch="release/$(cat .version)"
 
 remote=$(
-	git remote get-url origin | sed -E s"/https:\/\/(.*)/https:\/\/${GITHUB_TOKEN}@\1/g"
+  git remote get-url origin | sed -E s"/https:\/\/(.*)/https:\/\/${GITHUB_TOKEN}@\1/g"
 )
 
-echo $DOCKERHUB_TOKEN | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
 docker pull "$IMAGE:$GIT_COMMIT"
 docker tag "$IMAGE:$GIT_COMMIT" "$IMAGE:$VERSION"
@@ -27,9 +27,9 @@ git config --global user.name "CI Bot"
 git remote set-url origin "$remote"
 
 git checkout -b "$branch"
-echo "test" > flag
+echo "test" >flag
 git add flag
-git commit -m "Automated CI/CD commit"
+git commit -m "Automated CI/CD commit" -m "[skip ci]"
 git push origin "$branch"
 
 gh pr create -b "Please review" -t "Bump version to $(cat .version)"
