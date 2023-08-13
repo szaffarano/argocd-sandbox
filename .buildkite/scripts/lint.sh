2a93cd18d830d8e0c366f9236e145ca229fac68c
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-08-13 07:11:43 +0200

    Remove debugging

--- .buildkite/scripts/lint.sh
+++ .buildkite/scripts/lint.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-echo "---------- env ----------"
-env
-echo "---------- /env ----------"
-
-echo "------- I should not show GITHUB_TOKEN:${GITHUB_TOKEN}!!!"
-
 rustup component add rustfmt
 
 cargo fmt --all -- --check
