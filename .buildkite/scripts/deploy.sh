ea08b3e5247ee4f300576e3ecd9ec01a5a8940a6
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-08-16 18:02:18 +0200

    Start using buildkite hooks (#27)

--- .buildkite/scripts/deploy.sh
+++ .buildkite/scripts/deploy.sh
@@ -3,16 +3,6 @@
 set -euo pipefail
 
 function main {
-  base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-  source "$base_dir/common.sh"
-
-  setup
-  setup_docker_env
-
-  GIT_COMMIT="$(git_revision)"
-  VERSION="$(app_version)"
-  IMAGE="$(docker_image)"
-
   docker build \
     -t "$IMAGE:latest" \
     --build-arg VERSION="$VERSION" \
