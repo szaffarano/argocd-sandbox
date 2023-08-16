247a6f9f2e6dcd4c1a717a66991a0062d78dfa0c
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-08-16 17:57:08 +0200

    Start using buildkite hooks

--- .buildkite/scripts/release.sh
+++ .buildkite/scripts/release.sh
@@ -3,25 +3,22 @@
 set -euo pipefail
 
 function main {
-  base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-  source "$base_dir/common.sh"
+  local branch
+  local title
+  local k8s_base
 
-  setup
   setup_git_remote
-  setup_docker_env
 
-  GIT_COMMIT="$(git_revision)"
-  VERSION="$(app_version)"
-  IMAGE="$(docker_image)"
   branch="release/$VERSION"
   title="Bump version $VERSION"
+  k8s_base="infra/k8s/overlays/production"
 
   msg_file="$(mktemp)"
 
   cat <<EOF >"$msg_file"
-    Bump version $VERSION
+Bump version $VERSION
 
-    [skip ci]
+[skip ci]
 EOF
 
   echo ":release: About to release $VERSION"
@@ -40,16 +37,23 @@ EOF
 
   git checkout -b "$branch"
 
-  grep -lr newTag infra/k8s/overlays/production | while read -r f; do
+  grep -lr newTag "$k8s_base" | while read -r f; do
     sed -i s"/newTag: .*/newTag: $VERSION/g" "$f"
   done
 
-  git add infra/k8s/overlays/production
+  git add "$k8s_base"
 
   git commit -m "$title" -m "[skip ci]"
   git push origin "$branch"
 
   gh pr create -F "$msg_file" -t "$title"
+}
+
+function setup_git_remote {
+  remote=$(
+    git remote get-url origin | sed -E s"/https:\/\/(.*)/https:\/\/${GITHUB_TOKEN}@\1/g"
+  )
+  git remote set-url origin "$remote"
 }
 
 main "$@"
